8d8b6c912124baa981ed4152ddbd2dcd928f6c33
alessandro.carestia <carestiaal@gmail.com>
2023-02-17 15:32:30 +0100

    primo commit sito NOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
+import { MyFooterComponent } from './my-footer/my-footer.component';
+import { MyLoginComponent } from './my-login/my-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHeaderComponent,
+    MyFooterComponent,
+    MyLoginComponent
   ],
   imports: [
     BrowserModule
